d7a174f4ad5f99476901a4a965332d4ecd438f72
Craig Patten <cjp@cjp.id.au>
2015-09-12 01:56:14 +0930

    Initial Commit

---
+++ SJCLKit/SJCLKit.h
@@ -0,0 +1,19 @@
+//
+//  SJCLKit.h
+//  SJCLKit
+//
+//  Created by Craig Patten on 12/09/2015.
+//  Copyright © 2015 Moondust. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SJCLKit.
+FOUNDATION_EXPORT double SJCLKitVersionNumber;
+
+//! Project version string for SJCLKit.
+FOUNDATION_EXPORT const unsigned char SJCLKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SJCLKit/PublicHeader.h>
+
+
